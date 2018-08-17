bf34285acffcfb11aa5b60047903a7d86c28de45
PKM007 <37593274+PKM007@users.noreply.github.com>
2018-08-17 12:57:50 +0530

    Add files via upload

---
+++ CurrencyConvert.java
@@ -0,0 +1,10 @@
+package com.example.dell.currencycoverter;
+
+public class CurrencyConvert {
+    String ID;
+    Double converted_value;
+    String convert_to;
+    String convert_from;
+
+}
+
---
+++ MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.dell.currencycoverter;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import org.w3c.dom.Text;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText from_to;
+    Button submit;
+    TextView id, value, to, from;
+    private String con_val="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View popupView = getLayoutInflater().inflate(R.layout.activity_main, null);
+        from_to =popupView.findViewById(R.id.fromto);
+        submit =popupView.findViewById(R.id.submit);
+        id =popupView.findViewById(R.id.id);
+        value =popupView.findViewById(R.id.value);
+        to =popupView.findViewById(R.id.to);
+        from =popupView.findViewById(R.id.from);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number = from_to.getText().toString();
+                if (number.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    // create the new user!
+                    callRetro(number);
+                }
+
+            }
+        });
+    }
+    void callRetro(String number){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://free.currencyconverterapi.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
+
+        CurrencyConvertService currencyConvertService=retrofit.create(CurrencyConvertService.class);
+        currencyConvertService.getCoverted(number).enqueue(new Callback<CurrencyConvert>() {
+            @Override
+            public void onResponse(Call<CurrencyConvert> call, Response<CurrencyConvert> response) {
+                CurrencyConvert currencyConvert = response.body();
+                con_val= String.format("%f",currencyConvert.converted_value);
+                id.setText(currencyConvert.ID);
+                value.setText(con_val);
+                to.setText(currencyConvert.convert_to);
+                from.setText(currencyConvert.convert_from);
+
+            }
+
+            @Override
+            public void onFailure(Call<CurrencyConvert> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Connection Issue", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+
+
+
+
+
